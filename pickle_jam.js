c25ae28d033d2fb6e761fe749fdcd15ba3be55d7
Carin Meier <cmeier@gigasquidsoftware.com>
2013-03-15 16:14:50 -0400

    Get cursor stuff working

--- pickle_jam.js
+++ pickle_jam.js
@@ -17,9 +17,6 @@ if (Meteor.isClient) {
     'click': function () {
       Session.set("selected_pad", this._id);
       var pad = Pads.findOne(Session.get("selected_pad"));
-      if (pad){
-        Session.set("selected_text", pad.text);
-      }
     }
   });
 
@@ -34,16 +31,17 @@ if (Meteor.isClient) {
 
   Template.pad_edit.events({
     'keyup textarea' : function (event) {
-      Session.set("selected_text", event.target.value);
+
+      var elem = $("#pad-text")[0];
+      var caretPos = doGetCaretPosition(elem);
       var padid = Session.get("selected_pad");
       Pads.update(padid, {$set: {text:  event.target.value}});
       Meteor.flush();
+      setCaretPosition($("#pad-text")[0], caretPos);
     }
   });
 
-  Template.pad_edit.preserve({
-    'textarea': function (node) { return node.id; }
-  });
+;
 
 
 
@@ -51,13 +49,65 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    var pads = Pads.find();
-    pads.forEach(function (pad) {
-      Pads.remove(pad._id);
-    });
-    Pads.insert({name: "Cookie", text: "Given: I want a cookie."})
-    Pads.insert({name: "Pie", text: "Given: I want some pie."})
+    if (Pads.find().count() == 0){
+      Pads.insert({name: "Cookie", text: "Given: I want a cookie."});
+      Pads.insert({name: "Pie", text: "Given: I want some pie."});
+    }else{
+      var pads = Pads.find();
+      pads.forEach(function (pad) {
+        Pads.update(pad._id, {$set: {text: "Given: I want a cookie."}})
+      });
+    }
   });
 }
 
+function setCaretAtEnd(elem) {
+        var elemLen = elem.value.length;
+        // For IE Only
+        if (document.selection) {
+            // Set focus
+            elem.focus();
+            // Use IE Ranges
+            var oSel = document.selection.createRange();
+            // Reset position to 0 & then set at end
+            oSel.moveStart('character', -elemLen);
+            oSel.moveStart('character', elemLen);
+            oSel.moveEnd('character', 0);
+            oSel.select();
+        }
+        else if (elem.selectionStart || elem.selectionStart == '0') {
+            // Firefox/Chrome
+            elem.selectionStart = elemLen;
+            elem.selectionEnd = elemLen;
+            elem.focus();
+        } // if
+    } // SetCaretAtEnd()
+
+function doGetCaretPosition (ctrl) {
+	var CaretPos = 0;	// IE Support
+	if (document.selection) {
+	ctrl.focus ();
+		var Sel = document.selection.createRange ();
+		Sel.moveStart ('character', -ctrl.value.length);
+		CaretPos = Sel.text.length;
+	}
+	// Firefox support
+	else if (ctrl.selectionStart || ctrl.selectionStart == '0')
+		CaretPos = ctrl.selectionStart;
+	return (CaretPos);
+}
+function setCaretPosition(ctrl, pos){
+	if(ctrl.setSelectionRange)
+	{
+		ctrl.setSelectionRange(pos,pos);
+		ctrl.focus();
+	}
+	else if (ctrl.createTextRange) {
+		var range = ctrl.createTextRange();
+		range.collapse(true);
+		range.moveEnd('character', pos);
+		range.moveStart('character', pos);
+		range.select();
+	}
+}
 
